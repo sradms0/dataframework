79322a8c2a762dd70297498f4954fcfd4612e638
Scott Adams <sradms0@protonmail.com>
2026-01-08 12:56:07 -0500

    12: Add IRepository and non-implemented methods.

---
+++ src/Abstractions/Dataframework.Abstractions.Repositories/IRepository.cs
@@ -0,0 +1,191 @@
+﻿using System.Linq.Expressions;
+using Queueware.Dataframework.Abstractions.Primitives;
+
+namespace Queueware.Dataframework.Abstractions.Repositories;
+
+/// <summary>
+/// Defines a repository for querying and persisting entities of type <typeparamref name="TEntity"/>.
+/// </summary>
+/// <typeparam name="TId">
+/// The type of the entity identifier.
+/// </typeparam>
+/// <typeparam name="TEntity">
+/// The entity type managed by the repository.
+/// </typeparam>
+public interface IRepository<in TId, TEntity> where TEntity : IId<TId>
+{
+    /// <summary>
+    /// Returns the total number of entities in the repository.
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// The total number of entities.
+    /// </returns>
+    Task<int> CountAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the number of entities that satisfy the specified predicate.
+    /// </summary>
+    /// <param name="expression">
+    /// A predicate used to filter the entities.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// The number of matching entities.
+    /// </returns>
+    Task<int> CountAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Removes the specified entity from the repository.
+    /// </summary>
+    /// <param name="entity">
+    /// The entity to remove.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Removes multiple entities from the repository.
+    /// </summary>
+    /// <param name="entities">
+    /// The entities to remove.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task DeleteAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns an entity that satisfies the specified predicate, or <c>null</c> if no such entity exists.
+    /// </summary>
+    /// <remarks>
+    /// If more than one entity matches the predicate, the selection behavior is implementation-defined.
+    /// Use <see cref="FirstOrDefaultAsync(Expression{Func{TEntity, bool}}, CancellationToken)"/> when ordering matters.
+    /// </remarks>
+    /// <param name="expression">
+    /// A predicate used to filter the entities.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// A matching entity, or <c>null</c>.
+    /// </returns>
+    Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the first entity that satisfies the specified predicate, or <c>null</c> if no such entity exists.
+    /// </summary>
+    /// <param name="expression">
+    /// A predicate used to filter the entities.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// The first matching entity, or <c>null</c>.
+    /// </returns>
+    Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the entity with the specified identifier, or <c>null</c> if no such entity exists.
+    /// </summary>
+    /// <param name="id">
+    /// The entity identifier.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// The matching entity, or <c>null</c>.
+    /// </returns>
+    Task<TEntity?> GetByIdAsync(TId id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Returns the entities with the specified identifiers.
+    /// </summary>
+    /// <remarks>
+    /// Implementations may return fewer entities than identifiers provided if some identifiers do not exist.
+    /// The ordering of the returned entities is implementation-defined unless otherwise documented.
+    /// </remarks>
+    /// <param name="ids">
+    /// The entity identifiers.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    /// <returns>
+    /// A collection of entities matching the provided identifiers.
+    /// </returns>
+    Task<IEnumerable<TEntity>> GetByIdAsync(IEnumerable<TId> ids, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Inserts the specified entity into the repository.
+    /// </summary>
+    /// <param name="entity">
+    /// The entity to insert.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task InsertAsync(TEntity entity, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Inserts multiple entities into the repository.
+    /// </summary>
+    /// <param name="entities">
+    /// The entities to insert.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task InsertAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates the specified entity in the repository.
+    /// </summary>
+    /// <param name="entity">
+    /// The entity to update.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates multiple entities in the repository.
+    /// </summary>
+    /// <param name="entities">
+    /// The entities to update.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task UpdateAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates a single field of the specified entity with the given value.
+    /// </summary>
+    /// <typeparam name="TField">
+    /// The type of the field being updated.
+    /// </typeparam>
+    /// <param name="entity">
+    /// The entity to update.
+    /// </param>
+    /// <param name="field">
+    /// An expression identifying the field to update.
+    /// </param>
+    /// <param name="value">
+    /// The new value to assign to the field.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token to observe while waiting for the operation to complete.
+    /// </param>
+    Task UpdateAsync<TField>(TEntity entity, Expression<Func<TEntity, TField>> field, TField value, CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Dataframework.Infrastructure.Repositories/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using Queueware.Dataframework.Abstractions.DataSources;
+using Queueware.Dataframework.Abstractions.Primitives;
+using Queueware.Dataframework.Abstractions.Repositories;
+
+namespace Queueware.Dataframework.Infrastructure.Repositories;
+
+/// <inheritdoc />
+public class Repository<TId, TEntity, TDataContext>(IDataContextFactory<TDataContext> dataContextFactory)
+    : IRepository<TId, TEntity> where TEntity : IId<TId> where TDataContext : class, IDataContext
+{
+    /// <inheritdoc />
+    public Task<int> CountAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<int> CountAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task DeleteAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> expression, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<TEntity?> GetByIdAsync(TId id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task<IEnumerable<TEntity>> GetByIdAsync(IEnumerable<TId> ids, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task InsertAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task InsertAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task UpdateAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Task UpdateAsync<TField>(TEntity entity, Expression<Func<TEntity, TField>> field, TField value, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
